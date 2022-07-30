8e98a659827090d2935c5b07d669ef05134b918c
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2022-07-30 13:03:45 +0530

    chnaged state

--- src/States.js
+++ src/States.js
@@ -3,10 +3,10 @@ import {states,districts} from './data'
 
 export const States = ({className,styles,onChange}) => {
   const style={...styles}
-  const stateList = states.map(state=>{return<option value={state} key={state}>{state}</option>})
+  const stateList = states.map(state=><option value={state} key={state}>{state}</option>)
   return (
     <>
-    <input className={className} onChange={e=>{onChange(e.target.value)}} style={style?style:""} type="text" list='state-dist-input'/>
+    <input className={className} onChange={e=>onChange(e.target.value)} style={style?style:""} type="text" list='state-dist-input'/>
     <datalist id='state-dist-input'>
       {stateList}
     </datalist>
@@ -18,10 +18,10 @@ export const States = ({className,styles,onChange}) => {
 export const Districts = ({className,style,state,onChange}) => {
     const districtsList= districts.filter(elem=>elem.state===state)
     const currentDistricts= districtsList?.map(elem=>elem.dists)
-    const dists = currentDistricts[0]?.map(dis=>{ return <option key={dis}>{dis}</option>})
+    const dists = currentDistricts[0]?.map(dis=><option key={dis}>{dis}</option>)
   return (
     <>
-     <input className={className} onChange={(e)=>{onChange(e.target.value)}}  style={style?style:{width:'100%'}} type="text" list='dist-input'/>
+     <input className={className} onChange={(e)=>onChange(e.target.value)}  style={style?style:{width:'100%'}} type="text" list='dist-input'/>
     <datalist id='dist-input'>
       {dists}
     </datalist>
